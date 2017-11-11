640e08044e362a43481c463b122860acb0a9a635
Johnathan Budd <jbudd01@college.harvard.edu>
2017-11-11 15:56:59 -0800

    added append

--- history.js
+++ history.js
@@ -3,18 +3,41 @@ const publicClient = new gdax.PublicClient();
 const fs = require('fs');
 const csv = require('fast-csv');
 
-var ws = fs.createWriteStream('my.csv');
 
-// Get data 
-publicClient.getProductHistoricRates({'start':'2017-11-07', 'end':'2017-11-08','granularity': 3000},(error, response, data)=>{
-	if (error){
-		console.log("there was an error");
+var num_days = 10;
+var today = new Date();
+today.setDate(today.getDate());
+var start = new Date();
+var end = new Date();
+
+// Get data
+function get_data(i){
+	if(i < num_days){
+		start.setDate(today.getDate() - num_days+i-2);
+		end.setDate(today.getDate()-num_days+i-1);
+		publicClient.getProductHistoricRates({'start': start, 'end':end,'granularity': 86400},(error, response, data)=>{
+		if (error){
+			console.log("there was an error");
+		}
+		else{
+
+			csv
+				.writeToString(data,function(err,data){
+					fs.appendFile('my.csv',data+'\n',(err)=>{
+						if(err) throw err;
+						console.log("the data was appended");
+					});
+				})
+				
+
+			}
+		});
+		i+=1;
+		get_data(i);
+	}else{
+		i=0;
 	}
-	else{
+	
+};
 
-		csv
-			.write(data)
-			.pipe(ws);
-
-	}
-});
+get_data(0);
